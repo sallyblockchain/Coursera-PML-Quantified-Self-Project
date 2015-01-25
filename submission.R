663050ca70ead7c5de81bc7e445898b97f6b43e0
Xiaodan Zhang <zhang349@illinois.edu>
2015-01-25 11:10:13 -0600

    First commit for the report testing page

---
+++ submission.R
@@ -0,0 +1,9 @@
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i], file=filename, quote=FALSE,
+                row.names=FALSE, col.names=FALSE)
+  }
+}
+pml_write_files(answers)
